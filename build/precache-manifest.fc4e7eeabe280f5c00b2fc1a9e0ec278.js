9938fbf43cf166f3a06e9e407e67ddd556e7aee4
vikas bansal <vikas.bansal@wynk.in>
2019-11-08 16:09:04 +0530

    added build folder for gitpages

---
+++ build/precache-manifest.fc4e7eeabe280f5c00b2fc1a9e0ec278.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c167220623ab4e96ddca07729dc6f0f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "800b6708946b4e9e9374",
+    "url": "/static/css/main.83153181.chunk.css"
+  },
+  {
+    "revision": "42c71c86aa4d7e16a713",
+    "url": "/static/js/2.9df7b64b.chunk.js"
+  },
+  {
+    "revision": "800b6708946b4e9e9374",
+    "url": "/static/js/main.15cf2659.chunk.js"
+  },
+  {
+    "revision": "65aed3934c32d999db1f",
+    "url": "/static/js/runtime-main.b298c032.js"
+  }
+]);
